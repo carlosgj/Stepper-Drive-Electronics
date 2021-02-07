c89b5d0ff49a04708218012ea8a835710c7d12a9
Carlos Gross Jones <c.gross.jones@gmail.com>
2021-02-06 21:16:45 -0800

    Implementing SPI

--- SDE.X/SPI.c
+++ SDE.X/SPI.c
@@ -1,5 +1,87 @@
 #include "SPI.h"
 
 void SPIInit(void){
+    SSP1ADD = 7; //7 should result in 1 MHz with 32 MHz Fosc
     
+    //Set up SPI pin locations
+    APFCON0bits.SDO1SEL = TRUE; //SDO1 on RA6
+    
+    //Set SPI directions
+    MISOTRIS = INPUT;
+    MOSITRIS = OUTPUT;
+    
+    //Setup MSSP
+    SSP1STATbits.SMP = FALSE; //TODO
+    SSP1STATbits.CKE = FALSE; //TODO
+    SSP1CON1bits.CKP = FALSE; //TODO
+    SSP1CON1bits.SSPM = 0b0000; //SPI master, clock = Fosc/4
+    
+    //Set CS directions
+    MCTRIS = OUTPUT;
+    DRV1TRIS = OUTPUT;
+    DRV2TRIS = OUTPUT;
+    DRV3TRIS = OUTPUT;
+    
+    //Write CS pins high
+    MCLAT = TRUE;
+    DRV1LAT = TRUE;
+    DRV2LAT = TRUE;
+    DRV3LAT = TRUE;
+    
+    //Turn on SSP
+    SSP1CON1bits.SSPEN = TRUE;
+}
+
+unsigned char SPIXfer(SPIDest dest, unsigned char *txBuf, unsigned char *rxBuf, unsigned char count){
+    switch(dest){
+        case MC:
+            MCLAT = FALSE;
+            break;
+        case DRV1:
+            DRV1LAT = FALSE;
+            break;
+        case DRV2:
+            DRV2LAT = FALSE;
+            break;
+        case DRV3:
+            DRV3LAT = FALSE;
+            break;
+        default:
+            return 1; //TODO
+            break;
+    }
+    
+    unsigned char i;
+    unsigned char timeout;
+    for(i=0; i<count; i++){
+        SSP1IF = FALSE;
+        timeout = SPI_TIMEOUT;
+        SSP1BUF = txBuf[i];
+        while(!SSP1IF){
+            timeout--;
+            if(timeout==0){
+                break;
+                //TODO: error handling
+            }
+        }
+        rxBuf[i] = SSP1BUF;
+    }
+    
+    switch(dest){
+        case MC:
+            MCLAT = TRUE;
+            break;
+        case DRV1:
+            DRV1LAT = TRUE;
+            break;
+        case DRV2:
+            DRV2LAT = TRUE;
+            break;
+        case DRV3:
+            DRV3LAT = TRUE;
+            break;
+        default:
+            return 1; //TODO
+            break;
+    }
 }
--- SDE.X/SPI.h
+++ SDE.X/SPI.h
@@ -4,7 +4,31 @@
 #include <xc.h> 
 #include "common.h"
 
+#define SPI_TIMEOUT (255); //TODO
+
+#define MCLAT (LATAbits.LATA1)
+#define DRV1LAT (LATAbits.LATA3)
+#define DRV2LAT (LATBbits.LATB0)
+#define DRV3LAT (LATAbits.LATA4)
+
+#define MCTRIS (TRISAbits.TRISA1)
+#define DRV1TRIS (TRISAbits.TRISA3)
+#define DRV2TRIS (TRISBbits.TRISB0)
+#define DRV3TRIS (TRISAbits.TRISA4)
+
+#define MISOTRIS (TRISBbits.TRISB1)
+#define MOSITRIS (TRISAbits.TRISA6)
+
+enum SPIDest{
+    MC,
+    DRV1, 
+    DRV2, 
+    DRV3
+};
+
 void SPIInit(void);
+
+unsigned char SPIXfer(SPIDest dest, unsigned char *txBuf, unsigned char *rxBuf, unsigned char count);
 
 #endif
 
